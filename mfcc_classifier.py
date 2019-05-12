8158ee52c5fa258de29d9d05902307fd102810e6
salik2109 <salik2109@gmail.com>
2019-05-13 02:36:17 +0530

    PSM and ESM classification

--- mfcc_classifier.py
+++ mfcc_classifier.py
@@ -15,6 +15,8 @@ from sklearn import preprocessing
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, LSTM
+from keras.layers import Conv1D, MaxPool1D, GlobalAvgPool1D, Dropout, BatchNormalization, Dense
+from keras.regularizers import l2
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping,ReduceLROnPlateau,ModelCheckpoint,TensorBoard,ProgbarLogger
@@ -32,13 +34,13 @@ seed = 1000
 
 # seconds
 MAX_SOUND_CLIP_DURATION = 10
-CLASSES = ['murmur', 'normal']
+CLASSES = ['PSM', 'ESM']
 MAX_PATIENT = 12
 MAX_EPOCHS = 50
 BATCH_SIZE = 10
 
 
-def load_audio_data(folder, file_names, duration=3, sr=16000):
+def load_audio_data(folder, file_names, duration=3, sr=SAMPLE_RATE):
     input_length = sr*duration
     # function to load files and extract features
     # file_names = glob.glob(os.path.join(folder, '*.wav'))
@@ -46,9 +48,10 @@ def load_audio_data(folder, file_names, duration=3, sr=16000):
     for file_name in file_names:
         try:
             sound_file = folder+file_name
-            #print ("load file ", sound_file)
+            #print("load file ", sound_file)
             # use kaiser_fast technique for faster extraction
             X, sr = librosa.load(sound_file, sr=sr, duration=duration, res_type='kaiser_fast')
+            print(sr)
             dur = librosa.get_duration(y=X, sr=sr)
             # pad audio file same duration
             if round(dur) < duration:
@@ -68,48 +71,64 @@ def load_audio_data(folder, file_names, duration=3, sr=16000):
 
 def load_data_and_split():
 
-    if isfile('x_data.npy')==0 and isfile('y_data.npy')==0:
-        A_folder = INPUT_DIR + 'set_a/'
+    if isfile('x_data.npy') == 0 and isfile('y_data.npy') == 0:
+        # A_folder = INPUT_DIR + 'set_a/'
+        #
+        # A_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_a'), 'normal*.wav')
+        # A_normal_sounds = load_audio_data(folder=A_folder, file_names=A_normal_files, duration=MAX_SOUND_CLIP_DURATION)
+        # A_normal_labels = [1 for items in A_normal_sounds]
+        #
+        #
+        # A_murmur_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_a'), 'murmur*.wav')
+        # A_murmur_sounds = load_audio_data(folder=A_folder, file_names=A_murmur_files, duration=MAX_SOUND_CLIP_DURATION)
+        # A_murmur_labels = [0 for items in A_murmur_files]
+        #
+        #
+        # print("loaded dataset-a")
+        #
+        # B_folder = INPUT_DIR + 'set_b/'
+        #
+        # B_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_b'), 'normal*.wav')  # include noisy files
+        # B_normal_sounds = load_audio_data(folder=B_folder, file_names=B_normal_files, duration=MAX_SOUND_CLIP_DURATION)
+        # B_normal_labels = [1 for items in B_normal_sounds]
+        #
+        # B_murmur_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_b'), 'murmur*.wav')  # include noisy files
+        # B_murmur_sounds = load_audio_data(folder=B_folder, file_names=B_murmur_files, duration=MAX_SOUND_CLIP_DURATION)
+        # B_murmur_labels = [0 for items in B_murmur_files]
+        #
+        #
+        # print("loaded dataset-b")
+        #
+        # x_data = np.concatenate((A_normal_sounds, A_murmur_sounds,
+        #                          B_normal_sounds, B_murmur_sounds))
+        #
+        # y_data = np.concatenate((A_normal_labels, A_murmur_labels,
+        #                          B_normal_labels, B_murmur_labels))
+        #
+        # np.save('x_data.npy', x_data)
+        # np.save('y_data.npy', y_data)
+        # print('Saved the new x_data and y_data')
+        A_folder = INPUT_DIR + 'ESM/'
 
-        A_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_a'), 'normal*.wav')
+        A_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'ESM/'), '*.wav')
         A_normal_sounds = load_audio_data(folder=A_folder, file_names=A_normal_files, duration=MAX_SOUND_CLIP_DURATION)
         A_normal_labels = [1 for items in A_normal_sounds]
 
 
-        A_murmur_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_a'), 'murmur*.wav')
-        A_murmur_sounds = load_audio_data(folder=A_folder, file_names=A_murmur_files, duration=MAX_SOUND_CLIP_DURATION)
-        A_murmur_labels = [0 for items in A_murmur_files]
+        print("loaded ESM dataset")
 
-        # test files
-        A_unlabelledtest_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_a'), 'Aunlabelledtest*.wav')
-        A_unlabelledtest_sounds = load_audio_data(folder=A_folder, file_names=A_unlabelledtest_files,
-                                                 duration=MAX_SOUND_CLIP_DURATION)
-        A_unlabelledtest_labels = [-1 for items in A_unlabelledtest_sounds]
+        B_folder = INPUT_DIR + 'PSM/'
 
-        print("loaded dataset-a")
-
-        B_folder = INPUT_DIR + 'set_b/'
-
-        B_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_b'), 'normal*.wav')  # include noisy files
+        B_normal_files = fnmatch.filter(os.listdir(INPUT_DIR + 'PSM/'), '*.wav')  # include noisy files
         B_normal_sounds = load_audio_data(folder=B_folder, file_names=B_normal_files, duration=MAX_SOUND_CLIP_DURATION)
         B_normal_labels = [1 for items in B_normal_sounds]
 
-        B_murmur_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_b'), 'murmur*.wav')  # include noisy files
-        B_murmur_sounds = load_audio_data(folder=B_folder, file_names=B_murmur_files, duration=MAX_SOUND_CLIP_DURATION)
-        B_murmur_labels = [0 for items in B_murmur_files]
 
-        # test files
-        B_unlabelledtest_files = fnmatch.filter(os.listdir(INPUT_DIR + 'set_b'), 'Bunlabelledtest*.wav')
-        B_unlabelledtest_sounds = load_audio_data(folder=B_folder, file_names=B_unlabelledtest_files,
-                                                 duration=MAX_SOUND_CLIP_DURATION)
-        B_unlabelledtest_labels = [-1 for items in B_unlabelledtest_sounds]
-        print("loaded dataset-b")
+        print("loaded PSM dataset")
 
-        x_data = np.concatenate((A_normal_sounds, A_murmur_sounds,
-                                 B_normal_sounds, B_murmur_sounds))
+        x_data = np.concatenate((A_normal_sounds, B_normal_sounds))
 
-        y_data = np.concatenate((A_normal_labels, A_murmur_labels,
-                                 B_normal_labels, B_murmur_labels))
+        y_data = np.concatenate((A_normal_labels, B_normal_labels))
 
         np.save('x_data.npy', x_data)
         np.save('y_data.npy', y_data)
@@ -135,10 +154,26 @@ def load_data_and_split():
 
 
 def build_model():
+    # model = Sequential()
+    # model.add(LSTM(units=32, return_sequences=True, input_shape=(40, 1)))
+    # model.add(LSTM(units=16, return_sequences=False))
+    # model.add(Dense(len(CLASSES), activation='softmax'))
+    # model.summary()
+    # return model
     model = Sequential()
-    model.add(LSTM(units=32, dropout=0.05, recurrent_dropout=0.20, return_sequences=True, input_shape=(40, 1)))
-    model.add(LSTM(units=16, dropout=0.05, recurrent_dropout=0.20, return_sequences=False))
-    model.add(Dense(len(CLASSES), activation='softmax'))
+    model.add(Conv1D(filters=4, kernel_size=9, activation='relu',
+                     input_shape=(40, 1),
+                     kernel_regularizer=l2(0.025)))
+    model.add(MaxPool1D(strides=4))
+    model.add(BatchNormalization())
+    model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Conv1D(filters=32, kernel_size=1, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.75))
+    model.add(GlobalAvgPool1D())
+    model.add(Dense(2, activation='softmax'))
     model.summary()
     return model
 
@@ -173,7 +208,7 @@ def evaluate_score(model, x_train, y_train, x_test, y_test):
 
     # Score prints the loss along with the metrics as mentioned in model.compile
     score = model.evaluate(x_test, y_test, verbose=1)
-    #print(len(score), score)
+    # print(len(score), score)
     print("model test data score        : ", round(score[1] * 100), "%")
 
 
